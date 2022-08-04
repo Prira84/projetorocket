6995cff007a43dfd600de9f17e4cacb6f9ac14c1
Priscila <prira84@gmail.com>
2022-08-04 00:39:15 -0300

    primeira versao

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native';
+import { VStack, Heading } from 'native-base';
+
+import Logo from '../assets/logo_primary.svg';
+import { Input } from '../components/Input';
+
 
 export function SignIn() {
     return (
-        <View>
-            <Text>Olá Priscila, seja bem vinda!</Text>
-        </View>
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo />
 
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+                Acesse sua conta
+            </Heading>
+
+            <Input />
+
+        </VStack>
     )
-}
+}   
